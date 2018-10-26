63e6a8771cb766e493d5fdea070e30e6ac11f06b
danilophyton123 <43479971+danilophyton123@users.noreply.github.com>
2018-10-26 15:34:59 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,3 +1,4 @@
+
 ###
 # Meu código
 
@@ -5,66 +6,111 @@ import tkinter as tk
 
 pedidos = [0]*12
 mesa_selecionada = 0
+entrada_de_pedido = None
 
 
 def click_mesa_01():
-    janela_de_pedido(1)
-
+    janela_de_pedido()
+    print("Mesa 1")
+    global mesa_selecionada 
+    mesa_selecionada = 1
 
 def click_mesa_02():
-    janela_de_pedido(2)
+    janela_de_pedido()
+    print("Mesa 2")
+    global mesa_selecionada 
+    mesa_selecionada = 2
+    
 
 
 def click_mesa_03():
-    janela_de_pedido(3)
+    janela_de_pedido()
+    print("Mesa 3")
+    global mesa_selecionada 
+    mesa_selecionada = 3
 
 
 def click_mesa_04():
-    janela_de_pedido(4)
+    janela_de_pedido()
+    print("Mesa 4")
+    global mesa_selecionada 
+    mesa_selecionada = 4
 
 
 def click_mesa_05():
-    janela_de_pedido(5)
+    janela_de_pedido()
+    print("Mesa 5")
+    global mesa_selecionada 
+    mesa_selecionada = 5
 
 
 def click_mesa_06():
-    janela_de_pedido(6)
+    janela_de_pedido()
+    print("Mesa 6")
+    global mesa_selecionada 
+    mesa_selecionada = 6
 
 
 def click_mesa_07():
-    janela_de_pedido(7)
-
+    janela_de_pedido()
+    print("Mesa 7")
+    global mesa_selecionada 
+    mesa_selecionada = 7
 
 def click_mesa_08():
-    janela_de_pedido(8)
+    janela_de_pedido()
+    print("Mesa 8")
+    global mesa_selecionada 
+    mesa_selecionada = 8
 
 
 def click_mesa_09():
-    janela_de_pedido(9)
+    janela_de_pedido()
+    print("Mesa 9")
+    global mesa_selecionada 
+    mesa_selecionada = 9
 
 
 def click_mesa_10():
-    janela_de_pedido(10)
+    janela_de_pedido()
+    print("Mesa 10")
+    global mesa_selecionada 
+    mesa_selecionada = 10
 
 
 def click_mesa_11():
-    janela_de_pedido(11)
+    janela_de_pedido()
+    print("Mesa 11")
+    global mesa_selecionada 
+    mesa_selecionada = 11
 
 
 def click_mesa_12():
-    janela_de_pedido(12)
+    janela_de_pedido()
+    print("Mesa 12")
+    global mesa_selecionada 
+    mesa_selecionada = 12
 
 
 def click_salvar():
-    global n_mesa
-    pedidos[n_mesa-1] += 10
+    global mesa_selecionada
+    global entrada_de_pedido
+    valor = float(entrada_de_pedido.get())
+    indice = mesa_selecionada - 1
+    pedidod[indice] += valor
 
 def janela_de_pedido():
 	global mesa_selecionada
+	global entrada_de_pedido
 	janela_pedido = tk.Toplevel()
-	tk.Label(janela_pedido, text= "Mesa {}")
+	tk.Label(janela_pedido, text="Mesa {}".format(mesa_selecionada)).pack()
+	tk.Label(janela_pedido, text="Valor do pedido:").pack()
+	entrada_de_pedido = tk.Entry(janela_pedido)
+	entrada_de_pedido.pack()
+	tk.Button(janela_pedido, text="Salvar", height=3, command=click_salvar).pack(fill=tk.X, pady=5)
+	tk.Button(janela_pedido, text="Encerrar mesa").pack(fill=tk.X)
 
-
+    
 
 
 
